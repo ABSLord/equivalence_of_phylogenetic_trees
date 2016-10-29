5b5939fcab8a0be5c3eae9d38602188f020fba4c
ABSLord <antonio.maksimovich@gmail.com>
2016-10-29 03:16:12 +0300

    fix main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from phylogenetic_trees import is_equals_phylogenetic_trees
 
 
-print(is_equals_phylogenetic_trees("tree1.txt", "tree2.txt"))
-print(is_equals_phylogenetic_trees("tree3.txt", "tree4.txt"))
+def test():
+    print(is_equals_phylogenetic_trees("tree1.txt", "tree2.txt"))
+    print(is_equals_phylogenetic_trees("tree3.txt", "tree4.txt"))
+
+
+test()
